ecc147ea9f1283956f54489bcdfab24c6228e1cc
agmsb <agmsb@google.com>
2021-04-15 16:17:58 +0000

    App and build command.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# cloudrun-vpc-proxy
+# Cloud Run as a VPC Proxy
+
+No more creating a VM and futzing around with SSH to get a client :)
+
+## Build with Buildpacks and Cloud build
+
+```
+ gcloud alpha builds submit --pack image=gcr.io/agmsb-k8s/cloudrun
+-vpc-proxy```
---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def landing_page():
+    return "Please use the path /ip_addr/<Private-IP> to use this proxy."
+
+@app.route("/ip_addr/<string:ip>")
+def proxy(ip):
+    return requests.get(ip).content
